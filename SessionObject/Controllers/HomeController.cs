1d122d407a04e1a1c1af7d0c693970e44e945eb2
kenan <kenan@LAPTOP-5KTTCAE1>
2021-05-25 12:59:04 +0200

    Add project files.

---
+++ SessionObject/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SessionObject.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace SessionObject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var userInfo = new UserInfo()
+            {
+                Id=10023,
+                Name="Timmi"
+            };
+            HttpContext.Session.SetString("SessionUser", JsonConvert.SerializeObject(userInfo));
+            return View();
+        }
+
+        public IActionResult WrongData()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ SessionObject/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SessionObject.Models;
+
+namespace SessionObject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("SessionUser")))
+            {
+                return View("WrongData");
+            }
+            else
+            {
+                var user = JsonConvert.DeserializeObject<UserInfo>(HttpContext.Session.GetString("SessionUser"));
+                return View(user);
+            }
+        }
+    }
+}
